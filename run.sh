129e2528bf1862f8ff6c359e556545c2e4952002
tamilacc <tamil.k.selvan@accenture.com>
2020-07-22 11:08:31 +0000

    [ci skip] Pushing deployment file for storageclass
    Signed-off-by: tamilacc <tamil.k.selvan@accenture.com>

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@ KUBECONFIG=/home/blockchain-automation-framework/build/config
 
 
 echo "Running the playbook..."
-exec ansible-playbook -vv /home/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+exec ansible-playbook -vvvvv /home/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
